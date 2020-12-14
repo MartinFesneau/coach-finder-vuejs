07bd3314fc705575f90985738e02be8b843fb7dc
Martin Fesneau <martin.fesneau@gmail.com>
2020-12-14 15:13:35 +0100

    send request to a coach

--- src/pages/requests/ContactCoach.vue
+++ src/pages/requests/ContactCoach.vue
@@ -2,39 +2,51 @@
   <form @submit.prevent="submitForm">
     <div class="form-control pt-4">
       <label class="label" for="email">Your email</label>
-      <input type="email" id="email" class="input" v-model.trim="email">
+      <input type="email" id="email" class="input" v-model.trim="email" />
     </div>
     <div class="form-controm">
       <label class="label" for="message">Your message</label>
-      <textarea id="message"  rows="5" class="text-area" v-model.trim="message"></textarea>
+      <textarea
+        id="message"
+        rows="5"
+        class="text-area"
+        v-model.trim="message"
+      ></textarea>
     </div>
-    <p class="errors" v-if="!formIsValid">Please enter a valid email or a non empty message</p>
+    <p class="errors" v-if="!formIsValid">
+      Please enter a valid email or a non empty message
+    </p>
     <div class="actions">
       <base-button>Send message</base-button>
     </div>
   </form>
-
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       email: '',
       message: '',
       formIsValid: true
-    }
+    };
   },
   methods: {
     submitForm() {
       this.formIsValid = true;
       if (this.email == '' || !this.email.includes('@') || this.message == '') {
         this.formIsValid = false;
-        return; 
+        return;
       }
+      this.$store.dispatch('requests/contactCoach', {
+        email: this.email,
+        message: this.message,
+        coachId: this.$route.id
+      });
+      this.$router.replace('/coaches');
     }
   }
-}
+};
 </script>
 
 <style scoped>
