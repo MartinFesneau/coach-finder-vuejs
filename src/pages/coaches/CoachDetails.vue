ad869113128c7db9fe4d8a4a1e54d5567c30d3e4
Martin Fesneau <martin.fesneau@gmail.com>
2020-12-14 15:00:33 +0100

    create contact form

--- src/pages/coaches/CoachDetails.vue
+++ src/pages/coaches/CoachDetails.vue
@@ -1,14 +1,15 @@
 <template>
   <section>
     <base-card>
-      <h2>{{ fullName }}</h2>
-      <h3>${{ rate }}/hour</h3>
+      <h2 class="h2">{{ fullName }}</h2>
+      <h3 class="h3">${{ rate }}/hour</h3>
+      <base-badge v-for="area in areas" :key="area" :type="`${area}-full`" :title="area"></base-badge>
     </base-card>
   </section>
   <section>
     <base-card>
     <header>
-      <h2>Interested ? Reach out now</h2>
+      <h2 class="h2">Interested ? Reach out now</h2>
       <base-button link :to="contactLink">Contact</base-button>
     </header>
     <router-view></router-view>
@@ -16,7 +17,6 @@
   </section>
   <section>
     <base-card>
-      <base-badge v-for="area in areas" :key="area" :type="area" :title="area"></base-badge>
       <p>{{ description }}</p>
     </base-card>
   </section>
