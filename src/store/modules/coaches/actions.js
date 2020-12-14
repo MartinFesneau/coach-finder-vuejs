2e26955d0b0e86b656abb3e2c9bb444225dc5484
Martin Fesneau <martin.fesneau@gmail.com>
2020-12-14 17:43:03 +0100

    send put request to firebase to register as coach

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,13 +1,30 @@
 export default {
-  addACoach(context, payload) {
-    const coach = {
-      id: context.rootGetters.userId ,
+  async addACoach(context, payload) {
+    const userId = context.rootGetters.userId
+    const coachData = {
       firstName: payload.first,
       lastName: payload.last,
       description: payload.desc,
       hourlyRate: payload.rate,
       areas: payload.areas
     };
-    context.commit('registerCoach', coach);
+    const response = await fetch(`https://vue-coach-finder-app-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json`,{
+      method: 'PUT',
+      body: JSON.stringify(coachData)
+    });
+    // async await is equivalent to the .then method, response will be created only once we received the response
+
+    // const responseData = await response.json();
+
+    if (!response.ok) {
+      // error
+    }
+
+
+
+    context.commit('registerCoach', {
+      ...coachData,
+      id: userId
+    });
   }
 };
