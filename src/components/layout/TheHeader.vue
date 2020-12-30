9ad4d8e4895afd82ef2bbfec164ad9eff3338043
Martin Fesneau <martin.fesneau@gmail.com>
2020-12-30 15:23:25 +0100

    add not found page

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -1,7 +1,7 @@
 <template>
-  <header class="bg-gray-300	">
-    <nav class="flex justify-between py-4 border-b-1 border-gray-400 block mx-72">
-      <h1 class="font-mono font-semibold text-xl">
+  <header class="bg-gray-300">
+    <nav class="flex justify-between py-4 border-b-1 border-gray-400 block mx-72 container mx-auto">
+      <h1 class="font-mono font-semibold text-xl font">
         <router-link to="/">Find a coach</router-link>
       </h1>
       <ul class="flex justify-evenly object-right">
